95f71d85c74c9fd9c83cfa566cdfe987f6dfaccf
Marius <mcouvratd@hibiscae.fr>
2023-09-22 18:16:28 +0200

    Adjustements

--- WindowSettings.h
+++ WindowSettings.h
@@ -13,7 +13,7 @@ public:
     static constexpr int WINDOW_WIDTH = 480;
     static constexpr int WINDOW_HEIGHT = 416;
     static constexpr float TIME_PER_FRAME = 1.f/60.f;
-    static constexpr std::string_view WINDOW_NAME = "Map!";
+    static constexpr std::string_view WINDOW_NAME = "BomberMan!";
 };
 
 
